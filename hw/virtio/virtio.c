8fb8d834b7f57dfef28c57161e8e0d1b159bba2b
Jinhua Cao <caojinhua1@huawei.com>
2022-02-10 16:59:16 +0800

    virtio-net: set the max of queue size to 4096
    Signed-off-by: Jinhua Cao <caojinhua1@huawei.com>

--- hw/virtio/virtio.c
+++ hw/virtio/virtio.c
@@ -2394,7 +2394,7 @@ VirtQueue *virtio_add_queue(VirtIODevice *vdev, int queue_size,
             break;
     }
 
-    if (i == VIRTIO_QUEUE_MAX || queue_size > VIRTQUEUE_MAX_SIZE)
+    if (i == VIRTIO_QUEUE_MAX)
         abort();
 
     vdev->vq[i].vring.num = queue_size;
