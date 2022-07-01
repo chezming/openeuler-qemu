5d1997031ee9f9536851ac0ab46aa758427005e7
Wu Yacheng <wuyacheng@wxiat.com>
2022-07-01 12:01:54 +0000

    sw64: add processing of the result of checkhmcode.sh
    We adds processing of the result of checkhmcode.sh in
    run_kvm_config.sh. If checkhmcode.sh returns an error, it will not
    configure for qemu.
    
    Signed-off-by: Wu Yacheng <wuyacheng@wxiat.com>

--- run_kvm_config.sh
+++ run_kvm_config.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+./checkhmcode.sh
+result=$?
+right=0
+if [ $result != $right ];then
+	exit 1
+fi
 ./configure --target-list=sw64-softmmu  --enable-spice --enable-kvm  --extra-cflags="-fpic -fPIC"  --extra-ldflags="-fpic -fPIC" --enable-debug --enable-seccomp  --disable-werror
