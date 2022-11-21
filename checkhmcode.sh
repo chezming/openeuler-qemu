ecafe0afe3711a02566edf1239ff3ba636b02f18
Wu Yacheng <wuyacheng@wxiat.com>
2022-11-21 10:09:25 +0000

    sw64: fix checkhmcode.sh according to new interface specification
    Modify the hmcode version acquisition and determination method, obtain
    the hmcode version number in a fixed format from a fixed location
    according to new interface specification, and determine the version
    number according to the incremental rule.
    
    Signed-off-by: Wu Yacheng <wuyacheng@wxiat.com>

--- checkhmcode.sh
+++ checkhmcode.sh
@@ -3,49 +3,63 @@ echo `date +"============= %Y-%m-%d-%H:%M:%S ================"`
 echo "We will check whether the hmcode-version of this machine is correct ..."
 echo "If the hmcode version is too old, please refer to https://developer.wxiat.com/understand/ecologial/37
 for the right hmcode version ..."
-result=$(hexdump -C -v -s 130816 -n 48  /dev/mem | cut -c 62-77 | grep "Ver" | cut -c 12-16 )
+result=$(hexdump -C -v -s 130816 -n 4  /dev/mem | cut -c 14-21 )
 fir=0
 sec=0
 thi=6
+inc=1
 if [[ "$result" != "" ]]
 then
-	array=(${result//./ })
-	if [ "" = "${array[0]}" ] || [ "" = "${array[1]}" ] || [ "" = "${array[2]}" ];then
+	array=(${result// / })
+	if [ "" = "${array[2]}" ] || [ "" = "${array[1]}" ] || [ "" = "${array[0]}" ];then
                 echo "error: hmcode version is too old , please upgrade your hmcode first !
 physical machine hmcode version must be higher than v1.1.6
 your local version is :"
-                echo $result
-                exit 1
+                echo ${array[2]}"."${array[1]}"."${array[0]}
+		exit 1
         fi
-	if [ ${array[0]} -gt $fir ];then
-		if [ ${array[1]} -gt $sec ];then
-			if [ ${array[2]} -gt $thi ];then
-				echo "Local hmcode version of physical machine is :"
-				echo $result
-				echo "version is ok ."
+	if [ ${array[2]} -gt $fir ];then
+		if [ ${array[2]} -gt $inc ];then
+			echo "Local hmcode version of physical machine is :"
+			echo ${array[2]}"."${array[1]}"."${array[0]}
+			echo "version is ok ."
+			exit 0
+		fi
+			if [ ${array[1]} -gt $sec ];then
+				if [ ${array[1]} -gt $inc ];then
+					echo "Local hmcode version of physical machine is :"
+					echo ${array[2]}"."${array[1]}"."${array[0]}
+					echo "version is ok ."
+					exit 0
+				fi
+					if [ ${array[0]} -gt $thi ];then
+						echo "Local hmcode version of physical machine is :"
+						echo ${array[2]}"."${array[1]}"."${array[0]}
+						echo "version is ok ."
+						exit 0
+					else
+						echo "error: hmcode version is too old , please upgrade your hmcode first !
+physical machine hmcode version must be higher than v1.1.6
+your local version is :"
+						echo ${array[2]}"."${array[1]}"."${array[0]}
+						exit 1
+					fi
+				else
+					echo "error: hmcode version is too old , please upgrade your hmcode first !
+physical machine hmcode version must be higher than v1.1.6
+your local version is :"
+					echo ${array[2]}"."${array[1]}"."${array[0]}
+					exit 1
+				fi
 			else
 				echo "error: hmcode version is too old , please upgrade your hmcode first !
 physical machine hmcode version must be higher than v1.1.6
 your local version is :"
-				echo $result
+				echo ${array[2]}"."${array[1]}"."${array[0]}
 				exit 1
 			fi
 		else
 			echo "error: hmcode version is too old , please upgrade your hmcode first !
-physical machine hmcode version must be higher than v1.1.6
-your local version is :"
-			echo $result
+physical machine hmcode version must be higher than v1.1.6"
 			exit 1
 		fi
-	else
-		echo "error: hmcode version is too old , please upgrade your hmcode first !
-physical machine hmcode version must be higher than v1.1.6
-your local version is :"
-		echo $result
-		exit 1
-	fi
-else
-	echo "error: hmcode version is too old , please upgrade your hmcode first !
-physical machine hmcode version must be higher than v1.1.6"
-	exit 1
-fi
