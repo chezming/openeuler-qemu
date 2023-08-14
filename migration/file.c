3e284a9e5776159686ef096d4d5e37f6ecb290ee
luofei <luo.fei3@neolink.com>
2023-08-14 16:57:35 +0800

    migrationi/file: append pid to file URI
    Libvirtd passes the standard file URI to qemu, qemu appends the pid
    to file URI to identify and distinguish, meanwhile delete the URI
    file when qemu live update complete.
    
    Signed-off-by: luofei <luo.fei3@neolink.com>

--- migration/file.c
+++ migration/file.c
@@ -12,24 +12,39 @@
 #include "io/channel-file.h"
 #include "io/channel-util.h"
 #include "trace.h"
+#include "sysemu/runstate.h"
+#include <unistd.h>
 
 void file_start_outgoing_migration(MigrationState *s, const char *filename,
                                    Error **errp)
 {
     g_autoptr(QIOChannelFile) fioc = NULL;
     QIOChannel *ioc;
+    pid_t pid;
+    char *filename_p;
 
-    trace_migration_file_outgoing(filename);
+    pid = getpid();
+    filename_p = g_strdup_printf("%s.%d", filename, pid);
+    trace_migration_file_outgoing(filename_p);
 
-    fioc = qio_channel_file_new_path(filename, O_CREAT | O_WRONLY | O_TRUNC,
+    fioc = qio_channel_file_new_path(filename_p, O_CREAT | O_WRONLY | O_TRUNC,
                                      0600, errp);
     if (!fioc) {
+        g_free(filename_p);
         return;
     }
 
     ioc = QIO_CHANNEL(fioc);
     qio_channel_set_name(ioc, "migration-file-outgoing");
     migration_channel_connect(s, ioc, NULL, NULL);
+    g_free(filename_p);
+}
+
+static void file_migrate_complete_unlink_file(void *opaque)
+{
+    char *filename = opaque;
+    unlink(filename);
+    g_free(filename);
 }
 
 static gboolean file_accept_incoming_migration(QIOChannel *ioc,
@@ -45,11 +60,17 @@ void file_start_incoming_migration(const char *filename, Error **errp)
 {
     QIOChannelFile *fioc = NULL;
     QIOChannel *ioc;
+    pid_t pid;
+    char *filename_p;
 
-    trace_migration_file_incoming(filename);
+    pid = getpid();
+    filename_p = g_strdup_printf("%s.%d", filename, pid);
+    trace_migration_file_incoming(filename_p);
 
-    fioc = qio_channel_file_new_path(filename, O_RDONLY, 0, errp);
+
+    fioc = qio_channel_file_new_path(filename_p, O_RDONLY, 0, errp);
     if (!fioc) {
+        g_free(filename_p);
         return;
     }
 
@@ -59,4 +80,11 @@ void file_start_incoming_migration(const char *filename, Error **errp)
                                file_accept_incoming_migration,
                                NULL, NULL,
                                g_main_context_get_thread_default());
+
+    /*
+     * Register Handler to delete VM state save file when
+     * qemu live update complete
+     */
+    qemu_add_cpr_exec_complete_handler(file_migrate_complete_unlink_file,
+                                       (void *)filename_p);
 }
